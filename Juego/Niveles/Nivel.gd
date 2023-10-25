b347cd6643d0c330ed332a437e3fc7cc36488ca1
Dictako <jorge.alan.331g@gmail.com>
2023-10-24 22:23:08 -0300

    Muchas cosas
    Agregue:
    -Personaje
    -Cañones
    -Proyectiles
    -Eventos

---
+++ Juego/Niveles/Nivel.gd
@@ -0,0 +1,27 @@
+class_name Nivel
+extends Node2D
+
+##Atributos onready
+onready var contenedor_disparos: Node
+
+
+##Metodos
+func _ready() -> void:
+	conectar_seniales()
+	crear_contededores()
+	
+
+
+##Metodos Customs
+func conectar_seniales() -> void:
+	Eventos.connect("disparo", self, "_on_disparando")
+	
+func crear_contededores() -> void:
+	contenedor_disparos = Node.new()
+	contenedor_disparos.name = "ContenedorDisparo"
+	add_child(contenedor_disparos)
+
+
+
+func _on_disparando(proyectil: Proyectil) -> void:
+	add_child(proyectil)
---
+++ Juego/Proyectiles/ProyectilBase.gd
@@ -0,0 +1,23 @@
+class_name Proyectil
+extends Area2D
+
+
+##Atributos
+var velocidad:Vector2 = Vector2.ZERO
+var danio:float = 0
+
+##Metodos
+func _physics_process(delta: float) -> void:
+	position += velocidad * delta
+
+
+##Constructor
+func crear(pos: Vector2, dir:float, vel:int, danio_p:int) -> void:
+	position = pos
+	rotation = dir
+	velocidad = Vector2(vel, 0).rotated(dir)
+
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
