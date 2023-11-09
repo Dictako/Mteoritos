6bdb4ba084ad31ec76770f41cdc2e831bbbedee2
Dictako <jorge.alan.331g@gmail.com>
2023-11-09 00:10:45 -0300

    area peligro
    asi es

--- Juego/Mteoritos/Mteorito.gd
+++ Juego/Mteoritos/Mteorito.gd
@@ -11,16 +11,33 @@ onready var impacto_sfx:AudioStreamPlayer = $AudioDestruir
 
 ##Atributos
 var puntos_vida: float = 10.0
+var esta_en_sector: bool = true setget set_esta_en_sector
+var pos_spawn_original:Vector2
+var vel_spawn_original:Vector2
 
 
 ##Metodos
 func _ready() -> void:
 	angular_velocity = vel_ang_base
 
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if esta_en_sector:
+		return
+	
+	var mi_transform:=state.get_transform()
+	mi_transform.origin = pos_spawn_original
+	linear_velocity = vel_spawn_original
+	state.set_transform(mi_transform)
+	esta_en_sector = true
+
+##Setters & Getters
+func set_esta_en_sector(valor: bool) -> void:
+	esta_en_sector = valor
 
 ##Constructor
 func crear(pos:Vector2, dir:Vector2, tamanio:float) -> void:
 	position = pos
+	pos_spawn_original = position
 	mass *= tamanio
 	$Sprite.scale = Vector2.ONE * tamanio
 	var radio:int = int($Sprite.texture.get_size().x /2.4 * tamanio)
@@ -28,6 +45,7 @@ func crear(pos:Vector2, dir:Vector2, tamanio:float) -> void:
 	forma_colision.radius = radio
 	$CollisionShape2D.shape = forma_colision
 	linear_velocity = (vel_lineal_base * dir / tamanio) * aleatorizar_velocidad()
+	vel_spawn_original = linear_velocity
 	angular_velocity = (vel_ang_base / tamanio) * aleatorizar_velocidad()
 	puntos_vida = puntos_vida_base * tamanio
 
---
+++ Juego/Peligros/SectorDePeligro.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+##Atributos expor
+export (String, "Vacio", "Mteorito", "Enemigo") var tipo_peligro
+export var numero_peligros: int = 10
+
+
+##Señales Internas
+func _on_SectorDePeligro_body_entered(body: Node) -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.1), "timeout")
+	enviar_seniales()
+
+
+func enviar_seniales()-> void:
+	Eventos.emit_signal(
+		"nave_en_sector_peligro",
+		$PosicionCentroSector.global_position,
+		tipo_peligro,
+		numero_peligros)
+	queue_free()
